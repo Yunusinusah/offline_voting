5ff034e257edb0c4f2039a7b25f37f20bf2bb1c9
Yunus <inusahmustapha428@gmail.com>
2025-09-17 15:09:26 +0000

    refactor: Update action field type in Log model to allow longer strings

--- backend/src/models/log.js
+++ backend/src/models/log.js
@@ -1,10 +1,23 @@
 module.exports = (sequelize, DataTypes) => {
-  return sequelize.define('Log', {
-    id: { type: DataTypes.INTEGER.UNSIGNED, primaryKey: true, autoIncrement: true },
-    user_id: { type: DataTypes.INTEGER },
-    voter_id: { type: DataTypes.INTEGER },
-    action: { type: DataTypes.ENUM('cast_vote','login','generate_token','update','delete','create') },
-    ip_address: { type: DataTypes.STRING(64) },
-    details: { type: DataTypes.TEXT }
-  }, { tableName: 'logs', timestamps: true, createdAt: 'created_at', updatedAt: false });
+  return sequelize.define(
+    "Log",
+    {
+      id: {
+        type: DataTypes.INTEGER.UNSIGNED,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      user_id: { type: DataTypes.INTEGER },
+      voter_id: { type: DataTypes.INTEGER },
+      action: { type: DataTypes.STRING(128) },
+      ip_address: { type: DataTypes.STRING(64) },
+      details: { type: DataTypes.TEXT },
+    },
+    {
+      tableName: "logs",
+      timestamps: true,
+      createdAt: "created_at",
+      updatedAt: false,
+    }
+  );
 };
